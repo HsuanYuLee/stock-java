851e40aacbf7b487cf9a9e080e8ad4528c2d3757
HsuanYuLee <a3216801@hotmail.com>
2018-03-13 21:33:06 +0800

    0.2 連接MySQL

--- src/main/java/org/test/Main.java
+++ src/main/java/org/test/Main.java
@@ -22,17 +22,15 @@ public class Main
         getdata.setUrl(new URL("http://www.twse.com.tw/exchangeReport/STOCK_DAY?response=html&date=20180201&stockNo=2330"));
         //初始化資料存取路徑
         getdata.setFileWriter(new FileWriter("/Users/USER/Desktop/data.txt",false));
+        //連到MySQL
+        getdata.connectMySQL();
         //下載資料
         getdata.download();
 
-
-
         /*
         建立伺服器
-        */
 
         Server server = new Server(8080);
-
         //建立Server內可調用的handler
         //並將handler裝進server內
         ServletHandler handler = new ServletHandler();
@@ -40,12 +38,13 @@ public class Main
 
         //將Servlet裝進Handler內
         handler.addServletWithMapping(HelloServlet.class, "/*");
+        //handler.addServletWithMapping(getdata.class, "/*");
 
         //啟動伺服器
         server.start();
         //
         server.join();
-
+        */
 
     }
 }
--- src/main/java/servlet/getdata/getdata.java
+++ src/main/java/servlet/getdata/getdata.java
@@ -8,40 +8,156 @@ import org.jsoup.select.Elements;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.*;
 
 public class getdata
 {
     private URL url;
+
+    private Connection conn;
+    private Statement statement;
+
+
     private FileWriter fileWriter;
 
 
-    public void setUrl(URL url) { this.url = url;}
-    public void setFileWriter(FileWriter fileWriter){this.fileWriter = fileWriter;}
 
-    public void download()throws IOException
+    public void setUrl(URL url)
+    {
+        this.url = url;
+        System.out.println("存入資料連結");
+    }
+    public void setFileWriter(FileWriter fileWriter)
+    {
+        this.fileWriter = fileWriter;
+        System.out.println("設定檔案位置");
+    }
+    public Connection connectMySQL()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/stock-history", "root", "12345");
+            System.out.println("連接成功MySQLToJava");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+
+    /*
+        public getdata() throws SQLException, IOException {
+        conn = connectMySQL();
+
+        ResultSet resultSet = null;
+        try {
+            statement = conn.createStatement();
+            resultSet = statement.executeQuery("SELECT name FROM world.country");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        while (resultSet.next())
+        {
+            String name = resultSet.getString("name");
+            System.out.println(name);
+        }
+
+        resultSet.close();
+        statement.close();
+        conn.close();
+
+    }
+
+     */
+
+    public void download() throws IOException, SQLException
     {
         Document data = Jsoup.parse(url, 5000);
-        Elements trs = data.select("tr");
+        ResultSet resultSet = null;
 
+        //建立表格
+        /*
+
+
+        try
+        {
+            statement = conn.createStatement();
+            statement.executeUpdate
+                    (
+                    "CREATE TABLE if not exists Stock_2330" +
+                            "(Date date," +
+                            "Trading_shares int," +
+                            "Turnover double," +
+                            "Opening_price double," +
+                            "Highest_price double," +
+                            "Lowest_price double," +
+                            "Closing_price double," +
+                            "Gross_Spread double," +
+                            "Number_of_transactions int);"
+                    );
+            System.out.println("表格建立成功");
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+          */
+
+
+        Elements trs = data.select("tr");
+        int i = 1;
         for (Element tr : trs)
         {
             Elements tds = tr.select("td");
 
-            int i = 0;
             for (Element td : tds)
             {
                 String Td = td.text();
                 System.out.println(Td + "\t");
                 fileWriter.write(Td + "\t");
-                i++;
             }
 
             System.out.println("\n");
             fileWriter.write("\n");
+            i++;
         }
 
         fileWriter.flush();
         fileWriter.close();
+
+        statement.close();
+        conn.close();
+
+
+
+        /*
+        while (resultSet.next())
+        {
+            String name = resultSet.getString("name");
+            System.out.println(name);
+        }
+
+        resultSet.close();
+        statement.close();
+        conn.close();
+
+
+
+
+
+
+
+
+
+        */
+
     }
+
+
+
 }
 
