855ed639192dcceabade98139eaf410cd2e36b7e
HsuanYuLee <a3216801@hotmail.com>
2018-03-20 21:22:30 +0800

    0.5 設定檔功能完成，開始建置伺服器部分

--- src/main/java/getdata/RunTodayData.java
+++ src/main/java/getdata/RunTodayData.java
@@ -1,14 +1,14 @@
 package getdata;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Calendar;
 import java.util.Date;
+import java.util.Properties;
 import java.util.TimerTask;
 
 public class RunTodayData extends TimerTask
 {
-
 
     @Override
     public void run()
@@ -17,23 +17,31 @@ public class RunTodayData extends TimerTask
 
         try
         {
+            Properties properties = new Properties();
+            FileInputStream FIS = new FileInputStream("C:/Users/USER/Documents/stock-java/src/config");
+            properties.load(FIS);
+
             /*
             歷史資料擷取
             預設台積電
             目前設定股票資料來源為證交所，：http://www.twse.com.tw/exchangeReport/STOCK_DAY?response=html&date=20180201&stockNo=2330
             */
-            String Stock_2330_Link = "http://www.twse.com.tw/exchangeReport/STOCK_DAY?response=html&date=********&stockNo=2330";
             RunStockData Get2330 = new RunStockData();
-            Get2330.setStocklink(Stock_2330_Link);
-            Get2330.setStockName("STOCK_2330");
+            Get2330.setStocklink(properties.getProperty("StockLink"));
+            Get2330.setStockName(properties.getProperty("StockID"));
             /*
             設定資料開始時間
             */
-            Get2330.setTime(2017,12);
+            Get2330.setTime
+                    (Integer.valueOf(properties.getProperty("Stock_Start_Year")),
+                            Integer.valueOf(properties.getProperty("Stock_Start_Month")));
             /*
             設定要連接的MySQL伺服器
             */
-            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/stock-history", "root", "12345");
+            Connection conn = DriverManager.getConnection
+                    (properties.getProperty("Stock_MySQL_URI"),
+                            properties.getProperty("MySQL_ID"),
+                            properties.getProperty("MySQL_PW"));
             Get2330.setMySQL(conn);
             Get2330.start();
 
@@ -45,17 +53,22 @@ public class RunTodayData extends TimerTask
             台灣加權指數(台指現貨)資料來源為證交所：http://www.twse.com.tw/indicesReport/MI_5MINS_HIST?response=html&date=20180301
             台指期(小台指)近月資料來源為鉅亨網：http://www.cnyes.com/futures/History.aspx?mydate=20180311&code=MTX1
             */
-            String MTX1Link = "http://www.cnyes.com/futures/History.aspx?mydate=********&code=MTX1";
             RunFuturesData GetMTX1 = new RunFuturesData();
-            GetMTX1.setFutureslink(MTX1Link);
+            GetMTX1.setFutureslink(properties.getProperty("FuturesLink"));
             /*
             設定資料開始時間
             */
-            GetMTX1.setTime(2018,3, 5);
+            GetMTX1.setTime
+                    (Integer.valueOf(properties.getProperty("Futures_Start_Year")),
+                            Integer.valueOf(properties.getProperty("Futures_Start_Month")),
+                            Integer.valueOf(properties.getProperty("Futures_Start_Day")));
             /*
             設定要連接的MySQL伺服器
             */
-            Connection conn2 = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/futures-history", "root", "12345");
+            Connection conn2 = DriverManager.getConnection
+                    (properties.getProperty("Futures_MySQL_URI"),
+                            properties.getProperty("MySQL_ID"),
+                            properties.getProperty("MySQL_PW"));
             GetMTX1.setMySQL(conn2);
             GetMTX1.start();
 
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,8 +5,10 @@ import org.eclipse.jetty.servlet.ServletHandler;
 import servlet.HelloServlet;
 import getdata.*;
 
+import java.io.FileInputStream;
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.Properties;
 import java.util.Timer;
 
 public class Main
@@ -15,6 +17,10 @@ public class Main
     {
         try
         {
+            Properties properties = new Properties();
+            FileInputStream FIS = new FileInputStream("C:/Users/USER/Documents/stock-java/src/config");
+            properties.load(FIS);
+
             //String Futuresdatalink1 = "http://www.twse.com.tw/indicesReport/MI_5MINS_HIST?response=html&date=********";
 
             //每日資料擷取
@@ -24,15 +30,14 @@ public class Main
             //設定開始執行時間，設定每日執行時間為下午兩點
             Calendar start = Calendar.getInstance(Locale.CHINESE);
             start.getTime();
-            start.set(Calendar.HOUR_OF_DAY,1);
-            start.set(Calendar.MINUTE,12);
-            start.set(Calendar.SECOND,0);
+            start.set(Calendar.HOUR_OF_DAY,Integer.valueOf(properties.getProperty("Timer_Start_Hour")));
+            start.set(Calendar.MINUTE,Integer.valueOf(properties.getProperty("Timer_Start_Minute")));
+            start.set(Calendar.SECOND,Integer.valueOf(properties.getProperty("Timer_Start_Second")));
 
             System.out.println("排程將在" + start.getTime() + "開始執行");
 
-            timer.schedule(runTodayData,start.getTime(), 86400000);
+            timer.schedule(runTodayData,start.getTime(), Integer.valueOf(properties.getProperty("Timer_Period")));
 
-            /*
             //建立伺服器
             Server server = new Server(8080);
             //建立Server內可調用的handler
@@ -46,7 +51,6 @@ public class Main
             server.start();
             //等待連線
             server.join();
-            */
 
         }catch (Exception e){ e.printStackTrace(); }
     }
